19a32baa1f40557543a292c9c4def28e424b4a96
גל שחר <galsh20@gmail.com>
2018-06-27 20:22:29 +0300

    all 3 forms

--- js/script.js
+++ js/script.js
@@ -3,14 +3,32 @@
 // var appUrl = "http://localhost:3000/";
 var appUrl = "https://karni-server.herokuapp.com/";
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['cloudinary']);
 
-app.controller('appCtrl', function($scope, $http, $anchorScroll) {
+app.run(function($http){   // Server Check
+    $http.post(appUrl+"serverCheck").then(function(data){
+        console.log("Server is On");
+    },
+    function(data, status) {
+        console.log("Server is Off");
+    });
+})
 
-    console.log("Karni - Client Ctrl");
-    $scope.formSuccess = false;
+/* @ngInject */
+function configure(CloudinaryProvider) {
+    CloudinaryProvider.configure({
+        cloud_name: 'dbq9ine3p',
+        api_key: '738281771945188'
+    });
+}
+angular.module('app').config(configure);
 
-    $scope.sendForm = function(){
+
+app.controller('questionnaireCtrl', function($scope, $http, $anchorScroll) {
+
+    console.log("Karni - Questionnaire Client Ctrl");
+
+    $scope.sendQuestionnaire = function(){
         console.log("sendForm function");
         // console.log($scope.fName+" "+$scope.lName+" "+$scope.idNo+" "+document.getElementById("bDate").value);
         if(($scope.fName)&&($scope.lName)&&($scope.idNo)&&(document.getElementById("bDate").value)){
@@ -118,13 +136,13 @@ app.controller('appCtrl', function($scope, $http, $anchorScroll) {
                 'ans24': $scope.ans24
             };
 
-            var siblingsCounter = 0;
-            for (var i = 0; i < userDetails.siblings.length; i++) {
-                if(userDetails.siblings[i].name!=null)
-                    siblingsCounter++;
-                if(i==userDetails.siblings.length-1)
-                    userDetails.siblings.splice(siblingsCounter);
-            }
+            // var siblingsCounter = 0;
+            // for (var i = 0; i < userDetails.siblings.length; i++) {
+            //     if(userDetails.siblings[i].name!=null)
+            //         siblingsCounter++;
+            //     if(i==userDetails.siblings.length-1)
+            //         userDetails.siblings.splice(siblingsCounter);
+            // }
             console.log("Request: "+JSON.stringify(userDetails, null, 4));
 
             $scope.formSuccess = true;
@@ -150,5 +168,135 @@ app.controller('appCtrl', function($scope, $http, $anchorScroll) {
         }
         else console.log("validate - error");
     }
+});
 
+app.controller('secrecyCtrl', function($scope, $http, $anchorScroll) {
+
+    console.log("Karni - Secrecy Client Ctrl");
+    $scope.formSuccess = false;
+    $scope.signature;
+    var signaturePad;
+
+    angular.element(function () {
+        console.log('page loading completed');
+
+        var canvas = document.getElementById('signature-pad');
+
+        function resizeCanvas() {
+            var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+            canvas.width = canvas.offsetWidth * ratio;
+            canvas.height = canvas.offsetHeight * ratio;
+            canvas.getContext("2d").scale(ratio, ratio);
+        }
+
+        window.onresize = resizeCanvas;
+        resizeCanvas();
+
+        signaturePad = new SignaturePad(canvas, {});
+
+        document.getElementById('clear').addEventListener('click', function () {
+            signaturePad.clear();
+        });
+
+    });
+
+    $scope.sendSecrecy = function(signImg){
+        console.log("sendSecrecy function");
+        // console.log($scope.fName+" "+$scope.lName+" "+$scope.idNo+" "+document.getElementById("bDate").value);
+        if(($scope.fName)&&($scope.idNo)&&($scope.parentName)&&($scope.parentId)){
+            console.log("validate - done");
+
+            if (signaturePad.isEmpty()) {
+                document.getElementById('signature-box').classList.add("signature-active");
+                document.getElementById('signature-box').scrollIntoView();
+                setTimeout(function(){ 
+                    document.getElementById('signature-box').classList.remove("signature-active");
+                }, 1200);
+                return;
+                // return alert(myVar+" Please provide a signature first.");
+            }
+            var data = signaturePad.toDataURL('image/png');
+
+
+            var userDetails = {
+                'fName': $scope.fName,
+                'idNo': $scope.idNo,
+                'parentName': $scope.parentName,
+                'parentId': $scope.parentId,
+                'img': data
+            };
+
+            console.log("Request: "+JSON.stringify(userDetails, null, 4));
+
+            $scope.formSuccess = true;
+            // scroll to top
+            var elmnt = document.getElementsByClassName("logo");
+            elmnt[0].scrollIntoView();
+
+            // // Post - Form
+            $http({
+                url: appUrl+"saveSecrecy",
+                method: "POST",
+                data: {userDetails: userDetails},
+                headers: {"Content-Type": "application/json"}  
+            }).then(function(response) {
+                // success
+                console.log("POST - sendSecrecy successfully");
+                console.log("response: "+JSON.stringify(response.data, null, 4));
+            }, 
+            function(response) { // optional
+                // failed
+                console.log("Error POST");
+            });
+
+        }
+        else console.log("validate - error");
+    }
+});
+
+app.controller('agreementCtrl', function($scope, $http, $anchorScroll) {
+
+    console.log("Karni - Agreement Client Ctrl");
+    $scope.formSuccess = false;
+
+    $scope.sendAgreement = function(){
+        console.log("sendAgreement function");
+
+        if(($scope.day)&&($scope.hour)&&(document.getElementById("startDate").value)){
+            var hours = $scope.hour.getHours();
+            var minutes = $scope.hour.getMinutes();
+            console.log("validate - done "+hours+":"+minutes);
+
+            var userDetails = {
+                'day': $scope.day,
+                'hour': hours+":"+minutes,
+                'startDate': document.getElementById("startDate").value
+            };
+
+            console.log("Request: "+JSON.stringify(userDetails, null, 4));
+
+            $scope.formSuccess = true;
+            // scroll to top
+            var elmnt = document.getElementsByClassName("logo");
+            elmnt[0].scrollIntoView();
+
+            // // Post - Form
+            $http({
+                url: appUrl+"saveAgreement",
+                method: "POST",
+                data: {userDetails: userDetails},
+                headers: {"Content-Type": "application/json"}  
+            }).then(function(response) {
+                // success
+                console.log("POST - sendAgreement successfully");
+                console.log("response: "+JSON.stringify(response.data, null, 4));
+            }, 
+            function(response) { // optional
+                // failed
+                console.log("Error POST");
+            });
+
+        }
+        else console.log("validate - error");
+    }
 });
